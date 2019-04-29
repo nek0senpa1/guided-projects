35db20164c14f74b5ae40678a33de112f0a9ed6d
koennecker <koenneckerb@gmail.com>
2019-04-29 10:09:20 -0700

    Create functions file

---
+++ intro-to-python-oop/intro-to-python-i/day-1/func.py
@@ -0,0 +1,19 @@
+def example_func(num):
+    for i in range(num):
+        print(i)
+
+
+example_func(10)
+
+a = ['a', 'b', 'c']
+
+def add_to_list(added_list):
+    new_list = added_list.copy()
+    new_list.append(1)
+    print(new_list)
+
+add_to_list(a)
+
+print(a)
+
+
