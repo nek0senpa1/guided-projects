b1b947445daaf66e0e8856903c1b5ef37f7b1c5a
koennecker <koenneckerb@gmail.com>
2019-04-29 10:09:43 -0700

    Create comps file

---
+++ intro-to-python-oop/intro-to-python-i/day-1/comps.py
@@ -0,0 +1,9 @@
+comp = [j for j in range(10)]
+
+comp2 = [k * 2 for k in range(10)]
+
+only_evens_multiplied = [k * 2 for k in range(10) if k % 2 == 0]
+
+print(comp)
+print(comp2)
+print(only_evens_multiplied)
