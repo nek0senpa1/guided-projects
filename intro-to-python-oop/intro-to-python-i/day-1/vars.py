c056d3d807c1b4c766f4b57140613d4dbd4c6213
koennecker <koenneckerb@gmail.com>
2019-04-29 10:08:07 -0700

    Create variables file

---
+++ intro-to-python-oop/intro-to-python-i/day-1/vars.py
@@ -0,0 +1,25 @@
+# const a = "a"
+# let a = "a"
+
+a = "a"
+a = 1
+
+# Like arrays
+a = ['a', 'b', 'c']
+
+# for i in a:
+#     print(i)
+
+# for j in range(10):
+#     print(j)
+
+# for k in range(10, -1, -1):
+#     print(k)
+
+# Tuples
+b = ('a', 'b')
+c = ('t',)
+
+for (index, e) in enumerate(a):
+    print(f"index: {index} element: {e}")
+
