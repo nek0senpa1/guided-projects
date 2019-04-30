f794388e3481f00c653aa2eadb6b448e115d4104
koennecker <koenneckerb@gmail.com>
2019-04-30 08:51:29 -0700

    Create intro-to-python-i day 2

--- intro-to-python-oop/intro-to-python-i/day-1/func.py
+++ intro-to-python-oop/intro-to-python-i/day-1/func.py
@@ -7,13 +7,17 @@ example_func(10)
 
 a = ['a', 'b', 'c']
 
+
 def add_to_list(added_list):
     new_list = added_list.copy()
     new_list.append(1)
     print(new_list)
+
 
 add_to_list(a)
 
 print(a)
 
 
+def func():
+    return func()
---
+++ intro-to-python-oop/intro-to-python-i/day-2/rps.py
@@ -0,0 +1,7 @@
+
+import random
+
+# Create a rock/paper/scissors REPL loop
+# Have a computer AI to play against us
+# Keep track of the score
+# Rules: r beats s, s beats p, p beats r
